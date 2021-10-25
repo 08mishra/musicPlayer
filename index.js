f711f4084963e77605a3e89dcf1958ebb47cdae8
prashant kumar <thearticle35@gmail.com>
2021-10-25 20:14:43 +0530

    added song titile in javascript

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let next = document.querySelector('#forward');
 let playing = document.querySelector('#play');
 let heading = document.querySelector('#music-heading');
 let img = document.querySelector('#img');
+let songName = document.querySelector('#songName');
 
 let trackIndex = 0;
 let isplaying = false;
@@ -12,81 +13,99 @@ let isplaying = false;
 let current_track = document.createElement('audio');
 
 let track_list = [{
-        path: 'music/01.mp3'
+        path: 'music/01.mp3',
+        musicName: 'jeene Lga hoon'
     },
 
     {
-        path: 'music/02.mp3'
+        path: 'music/02.mp3',
+        musicName: 'barish..'
+
     },
 
     {
-        path: 'music/03.mp3'
-    },
-
-
-    {
-        path: 'music/04.mp3'
-    },
-
-    {
-        path: 'music/05.mp3'
-    },
-
-    {
-        path: 'music/06.mp3'
-    },
-
-    {
-        path: 'music/07.mp3'
+        path: 'music/03.mp3',
+        musicName: 'ek mulakat ho..'
     },
 
 
     {
-        path: 'music/08.mp3'
+        path: 'music/04.mp3',
+        musicName: 'lo maan liya hmne'
+    },
+
+    {
+        path: 'music/05.mp3',
+        musicName: 'aaj ro len de'
+    },
+
+    {
+        path: 'music/06.mp3',
+        musicName: 'kaun tujhe..'
+    },
+
+    {
+        path: 'music/07.mp3',
+        musicName: 'jeena jeena..'
     },
 
 
     {
-        path: 'music/09.mp3'
+        path: 'music/08.mp3',
+        musicName: 'tu hi ye mujhko bta de'
     },
 
 
     {
-        path: 'music/10.mp3'
+        path: 'music/09.mp3',
+        musicName: 'jb tk..'
     },
 
 
     {
-        path: 'music/11.mp3'
+        path: 'music/10.mp3',
+        musicName: 'mere yara tere hm'
     },
 
 
     {
-        path: 'music/12.mp3'
+        path: 'music/11.mp3',
+        musicName: 'humdard'
     },
 
 
     {
-        path: 'music/13.mp3'
+        path: 'music/12.mp3',
+        musicName: 'junoon..'
     },
 
 
     {
-        path: 'music/14.mp3'
+        path: 'music/13.mp3',
+        musicName: 'tujhe jeena hai mere bina.'
     },
 
 
     {
-        path: 'music/15.mp3'
+        path: 'music/14.mp3',
+        musicName: 'aye dil hai muskil'
     },
 
 
     {
-        path: 'music/16.mp3'
+        path: 'music/15.mp3',
+        musicName: 'janu n mai tuhme mera..'
+    },
+
+
+    {
+        path: 'music/16.mp3',
+        musicName: 'chl wha jate hai..'
     },
 
     {
-        path: 'music/17.mp3'
+        path: 'music/17.mp3',
+        musicName: 'tere bina jeena kya..'
     }
 
 
@@ -99,6 +118,8 @@ function loadTrack(trackIndex) {
 
     heading.textContent = 'playing ' + (trackIndex + 1) + 'of ' + track_list.length;
     img.classList = 'rotate';
+    songName.textContent = track_list[trackIndex].musicName;
+
     current_track.addEventListener('ended', nextTrack);
 
 }
